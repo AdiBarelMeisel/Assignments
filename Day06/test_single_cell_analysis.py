760c5807bcfe471938a03b5574c6a9151c4374dd
Adi <adi5brel@gmail.com>
2024-06-04 22:05:12 +0300

    add new files

--- Day06/test_single_cell_analysis.py
+++ Day06/test_single_cell_analysis.py
@@ -1,10 +1,7 @@
 import pandas as pd 
-import pytest
 
 def test_single_cell():
     user_output = pd.read_csv('top5_genes.csv')
     ground_truth = pd.read_csv("validate_top5_genes.csv")
     pd.testing.assert_frame_equal(user_output, ground_truth)
 
-if __name__=='__main__':
-    test_single_cell()
